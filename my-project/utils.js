5a09f57d2fa5861a50844d7b112eca32370110fd
jvalvesdepaiva76 <jv.alvesdepaiva@gmail.com>
2024-10-02 18:58:14 -0300

    tirar comentarios desnecessarios

--- my-project/utils.js
+++ my-project/utils.js
@@ -1,10 +1,4 @@
-// utils.js
-
-// Função para selecionar países aleatórios
 export function getRandomCountries(countries, count) {
     const shuffled = [...countries].sort(() => 0.5 - Math.random());
     return shuffled.slice(0, count);
   }
-  
-  // Outras funções utilitárias podem ser adicionadas aqui no futuro
-  
